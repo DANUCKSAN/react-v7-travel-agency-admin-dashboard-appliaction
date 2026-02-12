ce315f91e5be0e9eb5688627401bc85951ffae39
DANUCKSAN <sathiyarajdanucksan@gmail.com>
2026-02-12 14:48:34 +1100

    primary-layout-done

--- app/routes/admin/all-users.tsx
+++ app/routes/admin/all-users.tsx
@@ -1,10 +1,16 @@
+import Header from 'components/Header'
 import React from 'react'
 
 const AllUsers = () => {
   return (
-    <div>
-      Hello
-    </div>
+    <main className='dashboard wrapper'>
+       <Header
+    title="Trips Page"
+    description="check out our current Users"
+    />
+
+    All Users Page Content
+    </main>
   )
 }
 
--- app/routes/admin/dashboard.tsx
+++ app/routes/admin/dashboard.tsx
@@ -1,10 +1,17 @@
+import Header from 'components/Header'
 import React from 'react'
 
 const Dashboard = () => {
+
+  const user={name:'Dan'};
   return (
-    <div>
-      Dashboard 
-    </div>
+   <main className='dashboard wrapper'>
+    <Header
+    title={`Welcome ${user?.name ?? 'Guest'} 👋`}
+    description="Track activity, trends and popular destinations in real time"
+    />
+Dashboard Page content 
+   </main>
   )
 }
 
