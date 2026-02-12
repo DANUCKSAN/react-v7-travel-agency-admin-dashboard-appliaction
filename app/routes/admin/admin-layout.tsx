900ed964984c7dbe8f846e71eb8b47a699a68576
DANUCKSAN <sathiyarajdanucksan@gmail.com>
2026-02-12 12:36:13 +1100

    nav-items-done

--- app/routes/admin/admin-layout.tsx
+++ app/routes/admin/admin-layout.tsx
@@ -1,12 +1,19 @@
 import React from 'react'
 import { Outlet } from 'react-router'
+import {SidebarComponent} from "@syncfusion/ej2-react-navigations";
+import NavItems from 'components/NavItems';
 
 const AdminLayout= () => {
   return (
     <div className='admin-layout'>
       Mobile Sidebar
       <aside className='w-full max-w-[270px] hidden lg:block'>
-        Sidebar
+      <SidebarComponent width={270} enableGestures={false}>
+
+         <NavItems/>
+     
+      </SidebarComponent>
+     
       </aside>
       <aside className='children'>
         <Outlet/>
