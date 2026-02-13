4b16afd6893c0b444c27217383df875f477dd656
DANUCKSAN <sathiyarajdanucksan@gmail.com>
2026-02-13 15:07:10 +1100

    redirect-auth-done

--- app/routes.ts
+++ app/routes.ts
@@ -1,6 +1,8 @@
 import { type RouteConfig, index, layout, route } from "@react-router/dev/routes";
 
 export default [
+
+    route('sign-in','routes/root/sign-in.tsx'),
     
     layout('routes/admin/admin-layout.tsx',[
      route('dashboard','routes/admin/dashboard.tsx'),
