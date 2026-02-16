b4d72da723fab058f2d9717872b1c724f8fe3f80
DANUCKSAN <sathiyarajdanucksan@gmail.com>
2026-02-16 16:12:52 +1100

    table-done

--- app/routes/admin/all-users.tsx
+++ app/routes/admin/all-users.tsx
@@ -2,6 +2,7 @@ import Header from 'components/Header'
 import React from 'react'
 import {ColumnDirective, ColumnsDirective, GridComponent} from '@syncfusion/ej2-react-grids'
 import { users } from '~/constants'
+import { cn } from 'lib/utils'
 
 const AllUsers = () => {
   return (
@@ -19,7 +20,7 @@ const AllUsers = () => {
       width="200"
       textAlign='Left'
       template={(props:UserData)=>(
-        <div className='flex items-center gap 1.5 px-4'>
+        <div className='flex items-center gap-1.5 px-4'>
           <img src={props.imageUrl} alt={props.name} className='rounded-full size-8 aspect-square'/>
           <span>{props.name}</span>
         </div>
@@ -34,7 +35,7 @@ const AllUsers = () => {
       headerText='Email'
       width="150"
       textAlign='Left'
-    />
+        />
 
     <ColumnDirective
       field='dateJoined'
@@ -54,6 +55,24 @@ const AllUsers = () => {
      
       
       />
+
+       <ColumnDirective
+      field='status'
+      headerText='Type'
+      width="100"
+      textAlign='Left'
+      template={(props:UserData)=>(
+     <article className={cn('status-column', props.status === 'user' ? 'bg-success-50': 'bg-light-300')}>
+                                <div className={cn('size-1.5 rounded-full',  props.status === 'user' ? 'bg-success-500': 'bg-gray-500')} />
+                                    <h3 className={cn('font-inter text-xs font-medium',  props.status === 'user' ? 'text-success-700' : 'text-gray-500')}>
+                                        {props.status}
+                                    </h3>
+                            </article>
+      )}
+        />
+
+
+
       </ColumnsDirective>
 
       
