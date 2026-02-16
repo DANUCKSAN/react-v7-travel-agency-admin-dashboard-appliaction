7fa5228c67e2c6dbf771094f4247b056624932cc
DANUCKSAN <sathiyarajdanucksan@gmail.com>
2026-02-16 16:41:22 +1100

    table-appwrite-data-done

--- app/routes/admin/all-users.tsx
+++ app/routes/admin/all-users.tsx
@@ -1,10 +1,19 @@
 import Header from 'components/Header'
 import React from 'react'
 import {ColumnDirective, ColumnsDirective, GridComponent} from '@syncfusion/ej2-react-grids'
-import { users } from '~/constants'
-import { cn } from 'lib/utils'
+import { cn, formatDate } from 'lib/utils';
+import { getAllUsers } from '~/appwrite/auth';
+import type { Route } from './+types/all-users';
 
-const AllUsers = () => {
+
+export const loader=async()=>{
+  const {users,total}=await getAllUsers(10,0);
+  return {users,total};
+}
+
+const AllUsers = ({loaderData}:Route.ComponentProps) => {
+  const {users}=loaderData;
+  
   return (
     <main className='all-users wrapper'>
        <Header
@@ -15,63 +24,44 @@ const AllUsers = () => {
     <GridComponent dataSource={users} gridLines='None'>
       <ColumnsDirective>
       <ColumnDirective
-      field='name'
-      headerText='name'
-      width="200"
-      textAlign='Left'
-      template={(props:UserData)=>(
-        <div className='flex items-center gap-1.5 px-4'>
-          <img src={props.imageUrl} alt={props.name} className='rounded-full size-8 aspect-square'/>
-          <span>{props.name}</span>
-        </div>
-      )}
-
-      
-      
-      />
-
-       <ColumnDirective
-      field='email'
-      headerText='Email'
-      width="150"
-      textAlign='Left'
-        />
-
-    <ColumnDirective
-      field='dateJoined'
-      headerText='Date Joined'
-      width="120"
-      textAlign='Left'
-         
-      
-      
-      />
-
-      <ColumnDirective
-      field='itineraryCreated'
-      headerText='Itinerary Created'
-      width="130"
-      textAlign='Left'
-     
-      
-      />
-
-       <ColumnDirective
-      field='status'
-      headerText='Type'
-      width="100"
-      textAlign='Left'
-      template={(props:UserData)=>(
-     <article className={cn('status-column', props.status === 'user' ? 'bg-success-50': 'bg-light-300')}>
-                                <div className={cn('size-1.5 rounded-full',  props.status === 'user' ? 'bg-success-500': 'bg-gray-500')} />
-                                    <h3 className={cn('font-inter text-xs font-medium',  props.status === 'user' ? 'text-success-700' : 'text-gray-500')}>
-                                        {props.status}
+                        field="name"
+                        headerText="Name"
+                        width="200"
+                        textAlign="Left"
+                        template={(props: UserData) => (
+                            <div className="flex items-center gap-1.5 px-4">
+        <img src={props.imageUrl} alt="user" className="rounded-full size-8 aspect-square" referrerPolicy="no-referrer" />
+                                <span>{props.name}</span>
+                            </div>
+                        )}
+                    />
+                <ColumnDirective
+                    field="email"
+                    headerText="Email Address"
+                    width="200"
+                    textAlign="Left"
+                />
+                    <ColumnDirective
+                    field="joinedAt"
+                    headerText="Date Joined"
+                    width="140"
+                    textAlign="Left"
+                    template={({joinedAt}: { joinedAt: string}) => formatDate(joinedAt)}
+                />
+                    <ColumnDirective
+                        field="status"
+                        headerText="Type"
+                        width="100"
+                        textAlign="Left"
+                        template={({ status }: UserData) => (
+                            <article className={cn('status-column', status === 'user' ? 'bg-success-50': 'bg-light-300')}>
+                                <div className={cn('size-1.5 rounded-full', status === 'user' ? 'bg-success-500': 'bg-gray-500')} />
+                                    <h3 className={cn('font-inter text-xs font-medium', status === 'user' ? 'text-success-700' : 'text-gray-500')}>
+                                        {status}
                                     </h3>
                             </article>
-      )}
-        />
-
-
+                        )}
+                    />
 
       </ColumnsDirective>
 
